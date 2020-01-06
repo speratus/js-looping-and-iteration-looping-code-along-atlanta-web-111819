dd592ee0e21baf595c82a38c6133d744c61edd9d
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-01-06 11:28:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names) {
+    const thanks = [];
+    for (let i = 0; i < names.length; i ++) {
+        thanks[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        console.log(thanks[i]);
+    }
+    return thanks;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        --num;
+    }
+}
